7a3261f14f1b7f7e4558ddd2b463c65ea57d445b
0n11san <Jon@Jons-MacBook-Pro.local>
2018-05-06 12:42:36 -0400

    added map functionality and beginnings of search modal

--- client/src/components/Map/App.js
+++ client/src/components/Map/App.js
@@ -9,7 +9,8 @@ class App extends Component {
     super();
     this.state = {
       name: 'React',
-      polygons: []
+      polygons: [],
+      markers:[],
     };
   }
 
@@ -20,19 +21,36 @@ class App extends Component {
        });
 
 
+       const url = [
+      // Length issue
+      `https://gist.githubusercontent.com`,
+      `/farrrr/dfda7dd7fccfec5474d3`,
+      `/raw/758852bbc1979f6c4522ab4e92d1c92cba8fb0dc/data.json`
+    ].join("")
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ markers: data.photos });
+      });
+
    }
 
   render() {
     return (
+
       <div className="App">
         <Map
+          markers={this.state.markers}
           polygons={this.state.polygons}
           googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyB1SJ3HV5ZGZkOfwO96Hku1mK2rl3sT_5I&libraries=geometry,drawing,places"
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={<div style={{ height: `400px` }} />}
           mapElement={<div style={{ height: `100%` }} />}
+
         />
       </div>
+
     );
   }
 }
